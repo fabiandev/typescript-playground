00b1a4c83d5c50034ed10b9f059b19fb95af4e3d
Fabian Pirklbauer <fabian.pirklbauer@bitmovin.com>
2018-06-08 09:59:22 +0200

    1.5.0 build

--- docs/proxy.js
+++ docs/proxy.js
@@ -1,2 +1,2 @@
-"use strict";self.MonacoEnvironment={baseUrl:"https://unpkg.com/monaco-editor@^0.10.0/min/"},importScripts("https://unpkg.com/monaco-editor@^0.10.0/min/vs/base/worker/workerMain.js");
+"use strict";self.MonacoEnvironment={baseUrl:"https://unpkg.com/monaco-editor@^0.13.1/min/"},importScripts("https://unpkg.com/monaco-editor@^0.13.1/min/vs/base/worker/workerMain.js");
 //# sourceMappingURL=proxy.js.map
