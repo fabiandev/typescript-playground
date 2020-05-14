34fd900dd427185d866873685ba543529efb7186
Fabian Leutgeb <hi@fabiandev.io>
2020-05-14 10:53:55 +0200

    1.10.0 build

--- docs/proxy.js
+++ docs/proxy.js
@@ -1,2 +1,2 @@
-"use strict";self.MonacoEnvironment={baseUrl:"https://unpkg.com/monaco-editor@^0.17.0/min/"},importScripts("https://unpkg.com/monaco-editor@^0.17.0/min/vs/base/worker/workerMain.js");
+"use strict";self.MonacoEnvironment={baseUrl:"https://unpkg.com/monaco-editor@^0.20.0/min/"},importScripts("https://unpkg.com/monaco-editor@^0.20.0/min/vs/base/worker/workerMain.js");
 //# sourceMappingURL=proxy.js.map
